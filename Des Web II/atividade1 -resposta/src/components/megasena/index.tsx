0a032b0cae1b10dd43344699e8c5b3773b5be4be
Eduardo270704 <luis270704@gmail.com>
2023-10-31 22:05:30 -0300

    Adicionado

--- Des Web II/atividade1 -resposta/src/components/megasena/index.tsx
+++ Des Web II/atividade1 -resposta/src/components/megasena/index.tsx
@@ -7,15 +7,39 @@ export default function Megasena() {
 
   return (
     <div className="mega-bloco-principal">
-      <div className="mega-bloco-loteria">
-        <img src={trevo} alt="Megasena" />
-        <span className="mega-nome-loteria">MEGA-SENA</span>
+      <div>
+        <div className="mega-bloco-loteria">
+          <img src={trevo} alt="Megasena" />
+          <span className="mega-nome-loteria">MEGA-SENA</span>
+        </div>
+        <div className="mega-bloco-estimativa">
+          <div className="mega-texto-estimativa">
+            Estimativa de prêmio do próximo concurso. Sorteio em{" "}
+            {megasena.dataApuracao}
+          </div>
+        </div>
+        <div className="mega-valor-estimativa">
+          {megasena.valorEstimadoProximoConcurso.toLocaleString("pt-br", {
+            style: "currency",
+            currency: "BRL",
+          })}
+        </div>
       </div>
       <div className="mega-bloco-direita">
-        Estimativa de prêmio do próximo concurso. Sorteio em
-        {megasena.dataApuracao}
+        <div className="mega-linha-bola">
+          {megasena.dezenas.map((dezena) => (
+            <div className="mega-bola" key={dezena}>
+              {dezena}
+            </div>
+          ))}
+        </div>
+        <div className="mega-texto-acumulou">
+          {megasena.acumulado ? "ACUMULOU!" : ""}
+        </div>
+        <div className="mega-data-concurso">
+          {`Concurso ${megasena.numeroDoConcurso} - ${megasena.dataPorExtenso}`}
+        </div>
       </div>
-      <div className="mega-bloco-estimativa">{megasena.valorEstimadoProximoConcurso}</div>
     </div>
   );
 }
